1b4e90496f8c826d798d2df70f84d82bf1af96e4
Marco <marcoguarato15@gmail.com>
2025-11-04 14:42:37 -0300

    teste rotas - funcionou

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from app import app
 from flask import render_template, request, redirect, url_for, make_response
 
-@app.route("/")
-def index():
-    return render_template('base.html')
+from routes import index
+
+# @app.route("/")
+# def index():
+#     return render_template('base.html')
