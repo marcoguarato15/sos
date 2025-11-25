759eb9098b7a6427bc2320d386d7254b897cb872
Marco <marcoguarato15@gmail.com>
2025-11-25 17:02:50 -0300

    Feature: update e delete Notas

--- app/services/nota_service.py
+++ app/services/nota_service.py
@@ -6,10 +6,22 @@ def get_notas():
     return notas
 
 def post_nota(nota):
-    n = Nota(titulo=nota.titulo, demanda_id=nota.demanda_id, prioridade_id=nota.prioridade_id, data_criacao=nota.data_criacao, ativo=True)
+    n = Nota(titulo=nota.titulo, demanda_id=nota.demanda_id, prioridade_id=nota.prioridade_id, data_criacao=nota.data_criacao, ativo=nota.ativo)
     db.session.add(n)
     db.session.commit()
 
+def put_nota(id, titulo, demanda_id, prioridade_id):
+    nota = Nota.query.filter_by(id=id).first()
+    nota.titulo = titulo
+    nota.demanda_id = demanda_id
+    nota.prioridade_id = prioridade_id
+    db.session.commit()
+
+def get_nota_by_id(id):
+    nota = Nota.query.filter_by(id=id).first()
+    return nota
+
 def del_nota(id):
-    Nota.query.filter_by(id=id).delete
+    nota = Nota.query.filter_by(id=id).first()
+    nota.ativo = False
     db.session.commit()
--- routes/nota/nota.py
+++ routes/nota/nota.py
@@ -20,15 +20,44 @@ def post_nota():
     prioridade = None
     if request.method == "POST":
         if (titulo := request.form.get("titulo")) and (demanda := request.form.get("demanda")) and (prioridade := request.form.get("prioridade")):
-            nota = Nota(titulo=titulo, demanda_id=demanda, prioridade_id=prioridade)
-            # try:
-            nota_service.post_nota(nota)
-            flash("Sucesso ao adicionar nota","success")
-                # return redirect(url_for('notas'))
-            # except Exception as e:
-            #     flash("Erro ao adicionar nota","error")
-            #     return redirect(url_for('post_nota', tit=titulo, pri=prioridade, dem=demanda))
-        else:
-            flash("Preencha os campos","error")
+            nota = Nota(titulo=titulo, demanda_id=demanda, prioridade_id=prioridade, ativo=True)
+            try:
+                nota_service.post_nota(nota)
+                flash("Sucesso ao adicionar nota","success")
+                return redirect(url_for('notas'))
+            except Exception as e:
+                flash("Erro ao adicionar nota","error")
+                return redirect(url_for('post_nota', tit=titulo, pri=prioridade, dem=demanda))
 
     return render_template("nota/post_nota.html", demandas=demandas, prioridades=prioridades, tit=titulo, dem=demanda, prio=prioridade)
+
+@app.route("/put/nota/<int:id>", methods=["GET", "POST"])
+@jwt_refresh
+def put_nota(id):
+    nota = nota_service.get_nota_by_id(id)
+    demandas = demanda_service.get_demandas_ativas()
+    prioridades = prioridade_service.get_prioridades()
+    if request.method == "POST":
+        print(request.form.get("prioridade_id"))
+        print(request.form.get("demanda_id"))
+        print(request.form.get("titulo"))
+        if (titulo := request.form.get('titulo')) and (demanda_id := request.form.get('demanda_id')) and (prioridade_id := request.form.get('prioridade_id')):
+            try:
+                nota_service.put_nota(id, titulo, demanda_id, prioridade_id)
+                flash("Sucesso ao alterar Nota","success")
+                return redirect(url_for('notas'))
+            except Exception:
+                flash("Falha ao alterar Nota","error")
+
+    return render_template("nota/put_nota.html", demandas=demandas, prioridades=prioridades, nota=nota)
+
+@app.route("/del/nota/<int:id>")
+@jwt_refresh
+def del_nota(id):
+    try:
+        nota_service.del_nota(id=id)
+        flash("Nota excluida com sucesso","success")
+    except Exception:
+        flash("Falha ao excluir nota","error")
+
+    return redirect(url_for('notas'))
